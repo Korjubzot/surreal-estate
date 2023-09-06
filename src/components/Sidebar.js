f751c8dc4cf8dfbe814404553d36cd5866d38fdd
Korjubzot <korjubzot@gmail.com>
2023-09-06 16:41:16 +0100

    Added basic search functionality

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,12 +1,14 @@
 /* eslint-disable import/no-extraneous-dependencies */
-import React from "react";
-import { Link, useLocation } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import "../styles/sidebar.css";
 import qs from "qs";
 
 const Sidebar = () => {
+  const location = useLocation();
+  const navigate = useNavigate();
   const buildQueryString = (operation, valueObj) => {
-    const { search } = useLocation();
+    const { search } = location;
 
     const currentQueryParams = qs.parse(search, { ignoreQueryPrefix: true });
 
@@ -21,57 +23,80 @@ const Sidebar = () => {
     });
   };
 
+  const [query, setQuery] = useState("");
+
+  const handleSearch = (event) => {
+    event.preventDefault();
+    const newQueryString = buildQueryString("query", {
+      title: { $regex: query },
+    });
+    navigate(newQueryString);
+  };
+
   return (
-    <ul className="sidebar">
-      <li>
-        <Link
-          to={buildQueryString("query", { city: "Manchester" })}
-          className="sidebar-link"
-        >
-          Manchester
-        </Link>
-      </li>
-      <li>
-        <Link
-          to={buildQueryString("query", { city: "Leeds" })}
-          className="sidebar-link"
-        >
-          Leeds
-        </Link>
-      </li>
-      <li>
-        <Link
-          to={buildQueryString("query", { city: "Sheffield" })}
-          className="sidebar-link"
-        >
-          Sheffield
-        </Link>
-      </li>
-      <li>
-        <Link
-          to={buildQueryString("query", { city: "Liverpool" })}
-          className="sidebar-link"
-        >
-          Liverpool
-        </Link>
-      </li>
-      <li>
-        <Link
-          to={buildQueryString("sort", { price: -1 })}
-          className="sidebar-link"
-        >
-          Price Descending
-        </Link>
-      </li>
-      <li>
-        <Link
-          to={buildQueryString("sort", { price: 1 })}
-          className="sidebar-link"
-        >
-          Price Ascending
-        </Link>
-      </li>
-    </ul>
+    <>
+      <div>
+        <form onSubmit={handleSearch}>
+          <input
+            type="text"
+            value={query}
+            placeholder="Search"
+            onChange={(e) => setQuery(e.target.value)}
+          />
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+      <ul className="sidebar">
+        <li>
+          <Link
+            to={buildQueryString("query", { city: "Manchester" })}
+            className="sidebar-link"
+          >
+            Manchester
+          </Link>
+        </li>
+        <li>
+          <Link
+            to={buildQueryString("query", { city: "Leeds" })}
+            className="sidebar-link"
+          >
+            Leeds
+          </Link>
+        </li>
+        <li>
+          <Link
+            to={buildQueryString("query", { city: "Sheffield" })}
+            className="sidebar-link"
+          >
+            Sheffield
+          </Link>
+        </li>
+        <li>
+          <Link
+            to={buildQueryString("query", { city: "Liverpool" })}
+            className="sidebar-link"
+          >
+            Liverpool
+          </Link>
+        </li>
+        <li>
+          <Link
+            to={buildQueryString("sort", { price: -1 })}
+            className="sidebar-link"
+          >
+            Price Descending
+          </Link>
+        </li>
+        <li>
+          <Link
+            to={buildQueryString("sort", { price: 1 })}
+            className="sidebar-link"
+          >
+            Price Ascending
+          </Link>
+        </li>
+      </ul>
+    </>
   );
 };
 
